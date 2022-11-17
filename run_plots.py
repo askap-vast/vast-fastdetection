0b2dbbaa5ff2e0793670b395d3b454b2e551aa58
Yuanming Wang <ywan3191@ada.physics.usyd.edu.au>
2022-11-17 15:48:15 +1100

    add run_plots.py

---
+++ run_plots.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+
+import sys
+import glob
+
+from vastfast.cube import Cube, Filter
+from vastfast import plot
+from vastfast.plot import Candidates, Products
+
+
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+sh = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s - %(levelname)s: %(message)s")
+sh.setFormatter(formatter)
+logger.handlers.clear()
+logger.addHandler(sh)
+
+
+## ========= input ================
+## deep image
+deepimage = sys.argv[-6]
+## catalgoue csv file contain interested source info 
+catalogue = sys.argv[-5]
+## the folder location with processed short images
+folder = sys.argv[-4]
+## beam number - format of "beam00"
+beam = sys.argv[-3]
+## output folder 
+outdir = sys.argv[-2]
+## output file name 
+name = sys.argv[-1]
+
+
+## ====================================
+## get the imagelist with correct order
+imagelist = []
+for size in ["?", "??", "???", "????"]:
+    # tmp = glob.glob(folder + f'{beam}_{size}.fits')
+    tmp = glob.glob(folder + f'image_{size}.fits') # for FRB field 
+    tmp.sort()
+    imagelist += tmp
+
+
+logger.info("Loading foler {}".format(folder))
+logger.info("Processing {} images...".format(len(imagelist)))
+logger.info(imagelist)
+
+
+# =====================
+# plot final candidates 
+logger.info("========= Plotting =============")
+
+# final_csv = "{}/{}_final.csv".format(outdir, name)
+final_csv = catalogue
+
+p = Products(final_csv)
+p.generate_slices(imagelist=imagelist, 
+                  savename='{}/{}_slices'.format(outdir, name))
+p.generate_cutout(fitsname=deepimage, 
+                  savename='{}/{}_deepcutout'.format(outdir, name))
+p.generate_lightcurve(imagelist=imagelist, 
+                      deepname=deepimage, 
+                      savename='{}/{}_lightcurve'.format(outdir, name))
+
+
+
+
+
+logger.info("====== Finished. =====")
+
+
+
+
+
+
+
+
+
+
+
+
+
