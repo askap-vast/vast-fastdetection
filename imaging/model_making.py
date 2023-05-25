561d11e6425b8f2bc82a3b14fe51fd42bc249868
Yuanming Wang <ywan3191@macbook.com>
2023-05-25 21:55:57 +1000

    modify imaging parameters

--- imaging/model_making.py
+++ imaging/model_making.py
@@ -14,7 +14,7 @@ cell = ['2.5arcsec']
 deconvolver = 'mtmfs'
 nterm = 2
 scales = [0, 5, 10, 15, 25]
-niter = 3000
+niter = 5000
 weighting = 'briggs'  # natural, uniform, briggs
 robust = 0.5
 uvrange = '>1.0m'  # >1.0m
@@ -22,85 +22,85 @@ pbcor = False  # True, False
 pblimit = -0.2  # 0.1, -0.2
 
 
-# # reset previous stored model (if there is)
-# clearcal(vis=VIS)
-# print('Reset all model and corrected data')
+# reset previous stored model (if there is)
+clearcal(vis=VIS)
+print('Reset all model and corrected data')
 
 
-# # make a deep image
-# tclean(
-#     vis=VIS,
-#     selectdata=True,
-#     field='',
-#     spw='',
-#     timerange='',
-#     uvrange=uvrange,
-#     antenna='',
-#     scan='',
-#     observation='',
-#     intent='',
-#     datacolumn='data',
-#     imagename=imagename,
-#     imsize=imsize,
-#     cell=cell,
-#     phasecenter='',
-#     stokes='I',
-#     projection='SIN',
-#     specmode='mfs',
-#     reffreq='',
-#     nchan=-1,
-#     start='',
-#     width='',
-#     outframe='LSRK',
-#     veltype='radio',
-#     restfreq=[],
-#     interpolation='linear',
-#     gridder='widefield',
-#     facets=1,
-#     chanchunks=1,
-#     wprojplanes=-1,
-#     vptable='',
-#     aterm=True,
-#     psterm=False,
-#     wbawp=True,
-#     conjbeams=False,
-#     cfcache='',
-#     computepastep=360.0,
-#     pblimit=pblimit,
-#     normtype='flatnoise',
-#     deconvolver=deconvolver,
-#     scales=scales,
-#     nterms=nterm,
-#     smallscalebias=0.0,
-#     restoration=True,
-#     restoringbeam=[],
-#     pbcor=pbcor,
-#     outlierfile='',
-#     weighting=weighting,
-#     robust=robust,
-#     npixels=0,
-#     uvtaper=[],
-#     niter=niter,
-#     gain=0.1,
-#     threshold=0.0,
-#     cycleniter=-1,
-#     cyclefactor=1.0,
-#     minpsffraction=0.02,
-#     maxpsffraction=0.8,
-#     interactive=False,
-#     usemask='user',
-#     mask='',
-#     pbmask=0.0,
-#     savemodel='modelcolumn',
-#     startmodel='',
-#     parallel=False
-# )
-# print('Made a deep image finished %s' % BEAM)
+# make a deep image
+tclean(
+    vis=VIS,
+    selectdata=True,
+    field='',
+    spw='',
+    timerange='',
+    uvrange=uvrange,
+    antenna='',
+    scan='',
+    observation='',
+    intent='',
+    datacolumn='data',
+    imagename=imagename,
+    imsize=imsize,
+    cell=cell,
+    phasecenter='',
+    stokes='I',
+    projection='SIN',
+    specmode='mfs',
+    reffreq='',
+    nchan=-1,
+    start='',
+    width='',
+    outframe='LSRK',
+    veltype='radio',
+    restfreq=[],
+    interpolation='linear',
+    gridder='widefield',
+    facets=1,
+    chanchunks=1,
+    wprojplanes=-1,
+    vptable='',
+    aterm=True,
+    psterm=False,
+    wbawp=True,
+    conjbeams=False,
+    cfcache='',
+    computepastep=360.0,
+    pblimit=pblimit,
+    normtype='flatnoise',
+    deconvolver=deconvolver,
+    scales=scales,
+    nterms=nterm,
+    smallscalebias=0.0,
+    restoration=True,
+    restoringbeam=[],
+    pbcor=pbcor,
+    outlierfile='',
+    weighting=weighting,
+    robust=robust,
+    npixels=0,
+    uvtaper=[],
+    niter=niter,
+    gain=0.1,
+    threshold=0.0,
+    cycleniter=-1,
+    cyclefactor=1.0,
+    minpsffraction=0.02,
+    maxpsffraction=0.8,
+    interactive=False,
+    usemask='user',
+    mask='',
+    pbmask=0.0,
+    savemodel='modelcolumn',
+    startmodel='',
+    parallel=False
+)
+print('Made a deep image finished %s' % BEAM)
 
 
-# # subtract model
-# uvsub(vis=VIS)
-# print('Model-subtraction %s Finished.' % BEAM)
+# subtract model
+uvsub(vis=VIS)
+print('Model-subtraction %s Finished.' % BEAM)
 
 
 # convert image to fits file
