6f8339e7a5dc91d2d52bb10f502bb6048d7c6d70
Yuanming Wang <ywan3191@macbook.com>
2023-05-29 20:43:33 +1000

    test sleep

--- run_everything.sh
+++ run_everything.sh
@@ -11,27 +11,33 @@ VIS=$path/SB$sbid/visibilities
 MODELS=$path/SB$sbid/models
 IMGS=$path/SB$sbid/images
 
-mkdir $SCRIPTS
-mkdir $LOGS
-mkdir $path/SB$sbid
-mkdir $VIS
-mkdir $MODELS
-mkdir $IMGS
+echo $VIS
+echo $sbid
+
+# mkdir $SCRIPTS
+# mkdir $LOGS
+# mkdir $path/SB$sbid
+# mkdir $VIS
+# mkdir $MODELS
+# mkdir $IMGS
 
 
-module use /home/app/modulefiles/
-module load python/cpu-3.7.4
+# module use /home/app/modulefiles/
+# module load python/cpu-3.7.4
 
 
-python $loc/tools/get_everything_ready.py $sbid $SCRIPTS
+# python $loc/tools/get_everything_ready.py $sbid $SCRIPTS
 
-mkdir $VIS
-bash $SCRIPTS/download_selavy.sh
+# mkdir $VIS
+# bash $SCRIPTS/download_selavy.sh
 
 
 for i in {00..35}
 do
-    bash $SCRIPTS/submit_onebeam.sh beam$i
+    echo beam$i &
+    sleep 1s
+    # bash $SCRIPTS/submit_onebeam.sh beam$i &
+    # sleep 1s
 done
 
 
