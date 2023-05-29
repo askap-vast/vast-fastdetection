e22db9f08e1742dc8086993134570743600b3797
Yuanming Wang <ywan3191@macbook.com>
2023-05-29 20:46:04 +1000

    fix sleep unit

--- run_everything.sh
+++ run_everything.sh
@@ -11,33 +11,28 @@ VIS=$path/SB$sbid/visibilities
 MODELS=$path/SB$sbid/models
 IMGS=$path/SB$sbid/images
 
-echo $VIS
-echo $sbid
-
-# mkdir $SCRIPTS
-# mkdir $LOGS
-# mkdir $path/SB$sbid
-# mkdir $VIS
-# mkdir $MODELS
-# mkdir $IMGS
+mkdir $SCRIPTS
+mkdir $LOGS
+mkdir $path/SB$sbid
+mkdir $VIS
+mkdir $MODELS
+mkdir $IMGS
 
 
-# module use /home/app/modulefiles/
-# module load python/cpu-3.7.4
+module use /home/app/modulefiles/
+module load python/cpu-3.7.4
 
 
-# python $loc/tools/get_everything_ready.py $sbid $SCRIPTS
+python $loc/tools/get_everything_ready.py $sbid $SCRIPTS
 
-# mkdir $VIS
-# bash $SCRIPTS/download_selavy.sh
+mkdir $VIS
+bash $SCRIPTS/download_selavy.sh
 
 
 for i in {00..35}
 do
-    echo beam$i &
+    bash $SCRIPTS/submit_onebeam.sh beam$i &
     sleep 1s
-    # bash $SCRIPTS/submit_onebeam.sh beam$i &
-    # sleep 1s
 done
 
 
