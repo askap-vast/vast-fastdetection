cba5d7199f598fc0c53fa0c8495785dac4a5aeb9
Yuanming Wang <ywan3191@macbook.com>
2023-06-01 20:58:34 +1000

    test if statement

--- imaging/short_imaging.py
+++ imaging/short_imaging.py
@@ -35,22 +35,29 @@ tb.open(vis)
 ############################
 
 # the ASKAP resolution is 9.97s, so we need a different workaround for 10s images
-if ( step <= 10 ):
-    from collections import Counter
-    times = Counter(tb.getcol('TIME')).keys()
-    times.sort()
-    # time in fits is the middle time (but it doesn't matter as 10s is the sampling time, so it's the start, middle, and end time as well)
-    times = np.array(times) - step/2
-    print "Input time length is {}s, using a slightly different way to get short images".format(step)
+# if ( step <= 10 ):
+#     from collections import Counter
+#     times = Counter(tb.getcol('TIME')).keys()
+#     times.sort()
+#     # time in fits is the middle time (but it doesn't matter as 10s is the sampling time, so it's the start, middle, and end time as well)
+#     times = np.array(times) - step/2
+#     print "Input time length is {}s, using a slightly different way to get short images".format(step)
 
-else:
-    times = tb.getcol('TIME')
-    interval = tb.getcol('INTERVAL')[0]
-    # get the times array, change unit to second
-    # time in fits is therefore the START time 
-    times = np.arange(start=np.min(times)-interval/2,
-                      stop=np.max(times)+interval/2, step=step)
-    print "Input time length is {}s, i.e., {:.1f}m".format(step, step/60)
+# else:
+#     times = tb.getcol('TIME')
+#     interval = tb.getcol('INTERVAL')[0]
+#     # get the times array, change unit to second
+#     # time in fits is therefore the START time 
+#     times = np.arange(start=np.min(times)-interval/2,
+#                       stop=np.max(times)+interval/2, step=step)
+#     print "Input time length is {}s, i.e., {:.1f}m".format(step, step/60)
+
+from collections import Counter
+times = Counter(tb.getcol('TIME')).keys()
+times.sort()
+# time in fits is the middle time (but it doesn't matter as 10s is the sampling time, so it's the start, middle, and end time as well)
+times = np.array(times) - step/2
+print "Input time length is {}s, using a slightly different way to get short images".format(step)
 
 
 tb.close()
