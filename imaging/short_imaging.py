25d565a9caac7ba519dce1a7a81ab15882b1ea7f
Yuanming Wang <ywan3191@macbook.com>
2023-06-01 20:15:11 +1000

    clean up scripts

--- imaging/short_imaging.py
+++ imaging/short_imaging.py
@@ -1,25 +1,17 @@
-#!/usr/bin/env sh
 
+import os
 import sys
+import numpy as np
 
-ms_file = sys.argv[-1]
-print("** NOTICE  ** the arg3 passed to casa was :"+ms_file)
+vis = sys.argv[-3]
+imagename = sys.argv[-2]  # recommend in format of SBxxx_beamxx
+step = sys.argv[-1]  # images time length - in unit of seconds
+print("** NOTICE  ** the args passed to casa was :", vis, imagename, step)
 
-beam_idx_b = ms_file.find('beam') 
-beam_idx_e = ms_file.find('_', beam_idx_b + 4)
-beam_t = ms_file[beam_idx_b:beam_idx_e]
-beam_list = [beam_t]
-#epoch_list = ['15191']
-epoch_tag = 'scienceData_'
-epoch_idx_b = ms_file.find(epoch_tag)
-epoch_idx_e = ms_file.find('_', epoch_idx_b + len(epoch_tag))
-epoch_t = ms_file[epoch_idx_b+len(epoch_tag):epoch_idx_e]
-epoch_list = [epoch_t]
 
-# don't need to change part
 # corrected, data
 datacolumn = "corrected"
-imsize = 3000 # 4000
+imsize = 4096  # 3000? 4096? 2048?
 cell = ['2.5arcsec']
 uvrange = '>1m'
 # mtmfs, multiscale
@@ -33,60 +25,43 @@ niter = 0
 weighting = 'briggs'
 robust = 0.5
 
-# python pakeage
-import os
-import numpy as np
-from mpi4py import MPI
 
-# parallelly
-comm = MPI.COMM_WORLD
-num_process = comm.Get_size()
-rank = comm.Get_rank()
+# open table and read time column
+tb.open(filename)
 
-beam_split = np.array_split(beam_list, num_process)
+# the ASKAP resolution is 9.97s, so we need a different workaround for 10s images
+if step <= 10:
+    from collections import Counter
+    times = Counter(tb.getcol('TIME')).keys()
+    times.sort()
+    print("Input time length is {}s, using a slightly different way to get short images".format(step))
 
-# started to 15 minute imaging
-for i in range(len(beam_split[rank])):
-    beam = beam_split[rank][i]
-    print 'total %s, now %s, in rank %s (e.g. %s), length of this rank %s, num_process %s' % (len(beam_list), i, rank, beam, len(beam_split[rank]), num_process)
+else:
+    times = tb.getcol('TIME')
+    interval = tb.getcol('INTERVAL')[0]
+    # get the times array, change unit to second
+    # time in fits is therefore the START time 
+    times = np.arange(start=np.min(times)-interval/2,
+                      stop=np.max(times)+interval/2, step=step) + step/2
+    print("Input time length is {}s, i.e., {:.1f}m".format(step, step/60))
 
-    path_idx_e = ms_file.find("corrected_data")
-    path = ms_file[:path_idx_e]
-    #workdir1 = path + beam + "/"
-    workdir1 = path + "images/"
-    if not os.path.exists(workdir1):
-        os.mkdir(workdir1)
+tb.close()
 
-    for epoch in epoch_list:
-        #filename = '/o9000/ASKAP/VAST/ligo_variables/corrected_data/scienceData_SB%s_S190814bv.%s_averaged_cal.ms/' % (epoch, beam)
-        filename = ms_file
-        #workdir2 = '/o9000/ASKAP/VAST/%s/SB%s' % (beam, epoch)
-        workdir2 = workdir1 + epoch + "/"
-#        if not os.path.exists(workdir2):
-#            os.mkdir(workdir2)
-        os.chdir(workdir2)
 
-        # 15 minute imaging!!!!
-        # open table and read time column
-        tb.open(filename)
-        times = tb.getcol('TIME')
-        interval = tb.getcol('INTERVAL')[0]
-        # get the times array, change unit to second
-        times = np.arange(start=np.min(times)-interval/2, stop=np.max(times)+interval/2, step=15*60)
-        tb.close()
+# convert MJD to standard UTC
+for j in range(times.shape[0]):
+    stime = qa.time(qa.quantity(times[j]-step/2, 's'), form="ymd")[0]
+    etime = qa.time(qa.quantity(times[j]+step/2, 's'), form='ymd')[0]
+    imagename_j = '%s_%s' % (imagename, j)
+    timerange = '%s~%s' % (stime, etime)
 
-        # convert MJD to standard UTC
-        for j in range(times.shape[0]):
-            stime = qa.time(qa.quantity(times[j], 's'), form="ymd")[0]
-            etime = qa.time(qa.quantity(times[j]+15*60, 's'), form='ymd')[0]
-            imagename = '%s_%s' % (beam, j)
-            timerange = '%s~%s' % (stime, etime)
+    print '%s, %s' % (os.getcwd(), timerange)
 
-            print '%s, %s' % (os.getcwd(), timerange)
+    tclean(vis=filename, selectdata=True, field='', spw='', timerange=timerange, uvrange=uvrange, antenna='', scan='', observation='', intent='', datacolumn=datacolumn, imagename=imagename_j, imsize=imsize, cell=cell, phasecenter='', stokes='I', projection='SIN', startmodel='', specmode='mfs', reffreq='', nchan=-1, start='', width='', outframe='LSRK', veltype='radio', restfreq=[], interpolation='linear', gridder='widefield', facets=1, chanchunks=1, wprojplanes=-1, vptable='', aterm=True,
+           psterm=False, wbawp=True, conjbeams=False, cfcache='', computepastep=360.0, pblimit=-0.2, normtype='flatnoise', deconvolver=deconvolver, scales=scales, nterms=nterms, smallscalebias=0.0, restoration=True, restoringbeam=[], pbcor=False, outlierfile='', weighting=weighting, robust=robust, npixels=0, uvtaper=[], niter=niter, gain=0.1, threshold=0.0, cycleniter=-1, cyclefactor=1.0, minpsffraction=0.02, maxpsffraction=0.8, interactive=False, usemask='user', mask='', pbmask=0.0, savemodel=savemodel)
 
-            tclean(vis=filename, selectdata=True, field='', spw='', timerange=timerange, uvrange=uvrange, antenna='', scan='', observation='', intent='', datacolumn=datacolumn, imagename=imagename, imsize=imsize, cell=cell, phasecenter='', stokes='I', projection='SIN', startmodel='', specmode='mfs', reffreq='', nchan=-1, start='', width='', outframe='LSRK', veltype='radio', restfreq=[], interpolation='linear', gridder='widefield', facets=1, chanchunks=1, wprojplanes=-1, vptable='', aterm=True, psterm=False, wbawp=True, conjbeams=False, cfcache='', computepastep=360.0, pblimit=-0.2, normtype='flatnoise', deconvolver=deconvolver, scales=scales, nterms=nterms, smallscalebias=0.0, restoration=True, restoringbeam=[], pbcor=False, outlierfile='', weighting=weighting, robust=robust, npixels=0, uvtaper=[], niter=niter, gain=0.1, threshold=0.0, cycleniter=-1, cyclefactor=1.0, minpsffraction=0.02, maxpsffraction=0.8, interactive=False, usemask='user', mask='', pbmask=0.0, savemodel=savemodel)
-            # change to fits format
-            exportfits(imagename='%s.image'%imagename, fitsimage='%s.fits'%imagename)
+    # change to fits format
+    exportfits(imagename='%s.image' % imagename_j,
+               fitsimage='%s.fits' % imagename_j)
 
-    print '%s Finished. ' % beam
-
+print('%s Finished. ' % imagename)
