5d6715f66d39e9851658856f377ce58f3a8d7308
Yuanming Wang <ywan3191@macbook.com>
2023-06-03 13:21:08 +1000

    add IMGFAST slurm scripts

--- examples/submit_onebeam.sh
+++ examples/submit_onebeam.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-beam=$1 # in format of beam??
+beam=$1 # in format of beamxx
 
 # Download and untar visibility
 bash ~/vast_fastdetection/examples/bash_GETDATA_"$beam".sh
@@ -12,4 +12,7 @@ FIXDATA=`sbatch ~/vast_fastdetection/examples/slurm_FIXDATA_"$beam".sh | awk '{p
 # Create a deep sky model and subtract it
 MODELING=`sbatch -d afterok:${FIXDATA} ~/vast_fastdetection/examples/slurm_MODELING_"$beam".sh | awk '{print $4}'`
 
-echo scancel $FIXDATA $MODELING > /o9000/ASKAP/VAST/fast_survey/kill_"$beam"_jobs.sh
+# Generate short images 
+IMGFAST=`sbatch -d afterok:${MODELING} ~/vast_fastdetection/examples/slurm_IMGFAST_"$beam".sh | awk '{print $4}'`
+
+echo scancel $FIXDATA $MODELING $IMGFAST > /o9000/ASKAP/VAST/fast_survey/kill_"$beam"_jobs.sh
