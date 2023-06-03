5a89315a0d84bfdc2b871f170e957f242d2baca5
Yuanming Wang <ywan3191@macbook.com>
2023-06-03 13:30:18 +1000

    test run everything

--- run_everything.sh
+++ run_everything.sh
@@ -4,19 +4,25 @@ sbid=$1
 
 path=`pwd`
 loc='~/vast_fastdetection' # code location 
-SCRIPTS=$path/SCRIPTS
-LOGS=$path/LOGS
+
 
 VIS=$path/SB$sbid/visibilities
 MODELS=$path/SB$sbid/models
 IMGS=$path/SB$sbid/images
+CAND=$path/SB$sbid/candidates
 
-mkdir $SCRIPTS
-mkdir $LOGS
+SCRIPTS=$path/SB$sbid/SCRIPTS
+LOGS=$path/SB$sbid/LOGS
+
+
 mkdir $path/SB$sbid
 mkdir $VIS
 mkdir $MODELS
 mkdir $IMGS
+mkdir $CAND
+
+mkdir $SCRIPTS
+mkdir $LOGS
 
 
 module use /home/app/modulefiles/
@@ -25,15 +31,15 @@ module load python/cpu-3.7.4
 
 python $loc/tools/get_everything_ready.py $sbid $SCRIPTS
 
-cd $VIS
-bash $SCRIPTS/download_selavy.sh
+# cd $VIS
+# bash $SCRIPTS/download_selavy.sh
 
 
-for i in {00..35}
-do
-    bash $SCRIPTS/submit_onebeam.sh beam$i &
-    sleep 1s
-done
+# for i in {00..35}
+# do
+#     bash $SCRIPTS/submit_onebeam.sh beam$i &
+#     sleep 1s
+# done
 
 
 
