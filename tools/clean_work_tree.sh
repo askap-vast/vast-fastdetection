705b8b6c7d88ed3a25adb0f25c791d82fde5c9ca
Yuanming <wym20131028@gmail.com>
2023-10-09 15:33:39 +0800

    adding clean work tree scripts for one beam

---
+++ tools/clean_work_tree.sh
@@ -0,0 +1,13 @@
+
+
+# bash clean_work_tree.py 49156 20
+sbid=$1
+i=$2
+
+path=/o9000/ASKAP/VAST/fast_survey/SB$sbid
+echo cleaning work tree for SB$sbid beam$i in folder $path
+
+rm -r $path/data/*beam$i*
+rm -r $path/models/*beam$i*
+rm -r $path/images/*beam$i*
+rm -r $path/candidates/*beam$i*
