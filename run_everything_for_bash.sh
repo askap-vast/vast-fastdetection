1a2e493e69dafd59fd2c4b4e4a06b3b3e235fd81
Yuanming Wang <ywan3191@yoga-server.ipv6.vocus.com.au>
2023-10-09 19:51:12 +1100

    add bash version

---
+++ run_everything_for_bash.sh
@@ -0,0 +1,27 @@
+#!/bin/bash 
+
+sbid=$1
+num=18 # number of parallel processes 
+
+path=`pwd`/SB$sbid
+loc=$(dirname "$0")
+
+echo "The script you are running has:"
+echo "basename: [$(basename "$0")]"
+echo "dirname : [$(dirname "$0")]"
+echo "pwd     : [$(pwd)]"
+echo
+echo The output will store to $path
+echo The scripts are located in $loc
+
+
+SCRIPTS=$path/scripts
+
+python $loc/tools/get_everything_ready_for_bash.py $sbid $path
+bash $SCRIPTS/download_selavy.sh
+bash $SCRIPTS/bash_CHECKDATA.sh
+
+ls $SCRIPTS/bash_PROCESSING_beam*.sh | xargs -n 1 -t -P $num bash
+
+echo $sbid Finished!! 
+
